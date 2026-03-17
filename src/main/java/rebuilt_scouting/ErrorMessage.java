8218159a83bfd5143598aa08f1074affec47c094
DebuggingAndDragons <bussandrew730@gmail.com>
2026-03-17 13:32:09 -0400

    Add ErrorMessage Class

---
+++ src/main/java/rebuilt_scouting/ErrorMessage.java
@@ -0,0 +1,35 @@
+package rebuilt_scouting;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ErrorMessage {
+    public ErrorMessage(String errorMessage) {
+        if(!errorMessage.startsWith("Error: ")){
+            errorMessage=("Error: "+errorMessage);
+        }
+        JFrame error = new JFrame("ERROR!");
+        error.setLayout(new GridLayout(2, 1));
+
+        JLabel errorLabel = new JLabel(errorMessage);
+        JButton okButton = new JButton("Ok");
+
+        error.add(errorLabel);
+        error.add(okButton);
+        
+        error.setSize(300, 100);
+        error.setVisible(true);
+        error.setAlwaysOnTop(true);
+                
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                error.dispose();
+            }
+        });
+    }
+}
